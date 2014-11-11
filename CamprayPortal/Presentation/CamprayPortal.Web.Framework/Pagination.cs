8f0e422e47a12894966846c107f990482b05b0c4
alvisyu-campray <alvis.yu@campray.com>
2014-11-11 12:15:05 +0800

    News

---
+++ CamprayPortal/Presentation/CamprayPortal.Web.Framework/Pagination.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections.Generic;
+using CamprayPortal.Web.Framework.Mvc;
+
+namespace CamprayPortal.Web.Framework
+{
+    public class Pagination<T> where T : BaseNopEntityModel
+    {
+        private const int Splitindex = 2;
+
+        public Pagination()
+        {
+            PageItemList = new List<int>();
+        }
+
+        public Pagination(IList<T> modelItem, int currentIndex, int totalPage) : this()
+        {
+            this.ModelItem = modelItem;
+            this.CurrentIndex = currentIndex;
+            this.TotalPage = totalPage;
+            const int maxpagelist = Splitindex*2 + 1;
+
+            PrevIndex = currentIndex - 1 >= 1 ? currentIndex - 1 : 1;
+            NextIndex = totalPage - currentIndex >= 1 ? currentIndex + 1 : totalPage;
+
+            if (totalPage <= maxpagelist)
+            {
+                for (var i = 0; i < totalPage; i++)
+                {
+                    PageItemList.Add(i + 1);
+                }
+                FirstIndex = 1;
+                LastIndex = totalPage;
+            }
+            else
+            {
+
+                var vircurrentIndex = 0;
+
+                if (currentIndex - Splitindex <= 0)
+                    vircurrentIndex = Splitindex + 1;
+                else if (currentIndex + Splitindex > totalPage)
+                    vircurrentIndex = totalPage - Splitindex;
+                else
+                    vircurrentIndex = currentIndex;
+
+                var prespitindex = vircurrentIndex - Splitindex;
+                if (prespitindex <= 1)
+                {
+                    for (var i = 0; i < Splitindex; i++)
+                    {
+                        PageItemList.Add(i + 1);
+                    }
+                    FirstIndex = 1;
+                }
+                else
+                {
+                    for (var i = prespitindex; i < vircurrentIndex; i++)
+                    {
+                        PageItemList.Add(i);
+                    }
+                    FirstIndex = prespitindex - 1;
+                }
+
+                var nextspitindex = vircurrentIndex + Splitindex;
+                if (nextspitindex >= totalPage)
+                {
+                    for (var i = vircurrentIndex; i <= totalPage; i++)
+                    {
+                        PageItemList.Add(i);
+                    }
+                    LastIndex = totalPage;
+                }
+                else
+                {
+                    for (var i = vircurrentIndex; i <= nextspitindex; i++)
+                    {
+                        PageItemList.Add(i);
+                    }
+                    LastIndex = nextspitindex + 1;
+                }
+
+            }
+        }
+
+        public IList<T> ModelItem { get; set; }
+
+        public IList<int> PageItemList { get; set; }
+
+        public int CurrentIndex { get; set; }
+
+        public int TotalPage { get; set; }
+
+        public int FirstIndex { get; set; }
+        public int PrevIndex { get; set; }
+        public int NextIndex { get; set; }
+        public int LastIndex { get; set; }
+    }
+}
--- CamprayPortal/Presentation/CamprayPortal.Web/Controllers/AboutUsController.cs
+++ CamprayPortal/Presentation/CamprayPortal.Web/Controllers/AboutUsController.cs
@@ -1,9 +1,59 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using CamprayPortal.Core;
+using CamprayPortal.Core.Domain.News;
+using CamprayPortal.Services.Helpers;
+using CamprayPortal.Services.News;
+using CamprayPortal.Services.Seo;
+using CamprayPortal.Web.Framework;
+using CamprayPortal.Web.Models.AboutUs;
+using CamprayPortal.Web.Models.Common;
 
 namespace CamprayPortal.Web.Controllers
 {
-    public class AboutUsController : Controller
+    public class AboutUsController : BasePublicController
     {
+          private readonly IDateTimeHelper _dateTimeHelper;
+        private readonly INewsService _newsService;
+        private readonly IWorkContext _workContext;
+        private readonly IStoreContext _storeContext;
+
+        public AboutUsController(IDateTimeHelper dateTimeHelper,
+            INewsService newsService, 
+            IStoreContext storeContext,
+            IWorkContext workContext)
+        {
+            _dateTimeHelper = dateTimeHelper;
+            _newsService = newsService;
+            _storeContext = storeContext;
+            _workContext = workContext;
+        }
+
+        #region Utilities
+
+        [NonAction]
+        protected virtual void PrepareNewsItemModel(NewsItemModel model, NewsItem newsItem)
+        {
+            if (newsItem == null)
+                throw new ArgumentNullException("newsItem");
+
+            if (model == null)
+                throw new ArgumentNullException("model");
+
+            model.Id = newsItem.Id;
+            model.MetaTitle = newsItem.MetaTitle;
+            model.MetaDescription = newsItem.MetaDescription;
+            model.MetaKeywords = newsItem.MetaKeywords;
+            model.SeName = newsItem.GetSeName(newsItem.LanguageId, ensureTwoPublishedLanguages: false);
+            model.Title = newsItem.Title;
+            model.Short = newsItem.Short;
+            model.Full = newsItem.Full;
+            model.CreatedOn = _dateTimeHelper.ConvertToUserTime(newsItem.CreatedOnUtc, DateTimeKind.Utc);
+        }
+
+        #endregion
+
         public ActionResult HomePage()
         {
             return View();
@@ -37,10 +87,34 @@ namespace CamprayPortal.Web.Controllers
             return View();
         }
 
-        public ActionResult News()
+        public ActionResult News(int currentpage = 1)
         {
-            return View();
+            var newsItems = _newsService.GetAllNews(_workContext.WorkingLanguage.Id, _storeContext.CurrentStore.Id,
+                currentpage - 1,
+                4);
+            var model = newsItems
+                .Select(x =>
+                {
+                    var newsModel = new NewsItemModel();
+                    PrepareNewsItemModel(newsModel, x);
+                    return newsModel;
+                })
+                .ToList();
+
+            var pagination = new Pagination<NewsItemModel>(model, currentpage, newsItems.TotalPages);
+            return View(pagination);
         }
+
+
+        public ActionResult NewsDetail(int id = 0)
+        { 
+            var newsItem = _newsService.GetNewsById(id);
+            var newsModel = new NewsItemModel();
+            PrepareNewsItemModel(newsModel, newsItem);
+            return View(newsModel);
+        }
+
+
 
         public ActionResult ContactUs()
         {
