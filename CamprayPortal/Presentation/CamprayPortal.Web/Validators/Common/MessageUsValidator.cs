fb66305836ec6aaa033e0ea9f0b135f942162a5e
alvisyu-campray <alvis.yu@campray.com>
2014-11-11 14:02:16 +0800

    file add

---
+++ CamprayPortal/Presentation/CamprayPortal.Web/Validators/Common/MessageUsValidator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CamprayPortal.Web.Models.Common;
+using FluentValidation;
+
+namespace CamprayPortal.Web.Validators.Common
+{
+    public class MessageUsValidator : AbstractValidator<MessageUsModel>
+    {
+        public MessageUsValidator()
+        {
+            RuleFor(x => x.FirstName).NotEmpty();
+            RuleFor(x => x.LastName).NotEmpty();
+            RuleFor(x => x.CompanyName).NotEmpty();
+            RuleFor(x => x.EmailAddress).NotEmpty();
+        }
+    }
+}
