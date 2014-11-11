e2b58d049c4bde996f31bf71f77673a3eeb3d9e2
andy.lin <andy.lin@campray.com>
2014-11-11 10:31:25 +0800

    add files

---
+++ CamprayPortal/Libraries/CamprayPortal.Core/Domain/News/NewsType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CamprayPortal.Core.Domain.News
+{
+    public enum NewsType : int
+    {
+        News = 1,
+        Eevet = 2
+    }
+}
