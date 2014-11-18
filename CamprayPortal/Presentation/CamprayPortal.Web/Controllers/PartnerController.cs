0e1d9f694c25bcbd81300fa0c80d1d4356ad70b9
andy.lin <andy.lin@campray.com>
2014-11-18 12:27:55 +0800

    Add partner function

---
+++ CamprayPortal/Presentation/CamprayPortal.Web/Controllers/PartnerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CamprayPortal.Web.Controllers
+{
+    public class PartnerController : BasePublicController
+    {
+        // GET: Partner
+        public ActionResult Homepage()
+        {
+            return TopicModelView();
+        }
+    }
+}
