8f31dcfa5f27896d2986138ae9e22f6488742fca
alvisyu-campray <alvis.yu@campray.com>
2014-11-18 15:09:11 +0800

    regi

--- CamprayPortal/Presentation/CamprayPortal.Web/Controllers/DownloadsController.cs
+++ CamprayPortal/Presentation/CamprayPortal.Web/Controllers/DownloadsController.cs
@@ -1,12 +1,23 @@
 ﻿using System.Web.Mvc;
+using CamprayPortal.Core;
+using CamprayPortal.Core.Domain.Customers;
 
 namespace CamprayPortal.Web.Controllers
 {
     public class DownloadsController : BasePublicController
     {
+        private readonly IWorkContext _workContext;
+
+        public DownloadsController(IWorkContext workContext)
+        {
+            _workContext = workContext;
+        }
+
         // GET: Downloads
         public ActionResult Homepage()
         {
+            if (!_workContext.CurrentCustomer.IsRegistered())
+                return RedirectToRoute("Login");
             return TopicModelView();
         }
     }
